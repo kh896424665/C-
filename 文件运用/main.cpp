11ffcc0e2d99fc1fcba9984d907c8ae0858cead7
kh896424665 <896424665@qq.com>
2019-09-04 00:47:19 +0800

    www

---
+++ 文件运用/main.cpp
@@ -0,0 +1,22 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10];
+    ofstream outfile("f1.txt",ios::out);
+    if(!outfile)
+    {
+        cerr <<"open error!"<<endl;
+        exit(1);
+
+    }
+    cout<<"enter 10 integer numbers"<<endl;
+    for(int i=0;i<10;i++)
+    {
+        cin>>a[i];
+        outfile<<a[i]<<" ";
+    }
+    outfile.close();
+    return 0;
+}
